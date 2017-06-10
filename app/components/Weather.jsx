d27497d4fc168ffd3294ccdf84f741a43ea46a88
Chenghua <uguys@qq.com>
2017-06-10 12:25:51 -0700

    add styles to weather page

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -32,14 +32,14 @@ handleSearch: function(location){
 
       function renderMessage(){
         if(isLoading){
-          return <h3>Fetching Weather...</h3>;
+          return <h3 className="text-center">Fetching Weather...</h3>;
         }else if(temp && location){
           return <WeatherMessage temp={temp} location={location} />;
         }
       }
     return(
       <div>
-        <h3>Weather component</h3>
+        <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
       </div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -12,9 +12,9 @@ var React = require('react');
 // });
 
 var WeatherMessage = ({temp, location}) =>{
-  
+
   return(
-    <p>The temperature in {location} is {temp} F.</p>
+    <h3 className="text-center">The temperature in {location} is {temp} F.</h3>
   );
 };
 
